9190abc572e204ec59ca5fbd5ba525b9910816a6
Ra Lin <rasheedlindsey@live.com>
2023-11-27 15:33:40 -0500

    Seed edit

--- backend/db/seeders/20231117005118-demo-user.js
+++ backend/db/seeders/20231117005118-demo-user.js
@@ -10,18 +10,24 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await User.bulkCreate([
+    await User.bulkCreate(options, [
       {
+        firstName: 'Demo',
+        lastName: 'Lition',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Fake',
+        lastName: 'User1',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'User',
+        lastName: 'Fake2',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
