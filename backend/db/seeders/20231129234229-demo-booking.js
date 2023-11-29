f812a1d2598a247b1d646c73f3888238c9cc3412
Ra Lin <rasheedlindsey@live.com>
2023-11-29 18:51:49 -0500

    Booking Seeders done and done

---
+++ backend/db/seeders/20231129234229-demo-booking.js
@@ -0,0 +1,42 @@
+'use strict';
+const { Booking } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await Spot.bulkCreate([
+      {
+        spotId: 1,
+        userId: 1,
+        startDate: 10-1-2023,
+        endDate: 10-5-2023
+      },
+      {
+        spotId: 2,
+        userId: 2,
+        startDate: 11-5-2023,
+        endDate: 11-10-2023
+      },
+      {
+        spotId: 3,
+        userId: 3,
+        startDate: 11-25-2023,
+        endDate: 11-30-2023
+      },
+    ], { validate: true})
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Bookings';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [1, 2, 3] }
+    }, {});
+  }
+};
