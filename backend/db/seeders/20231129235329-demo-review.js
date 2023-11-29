ceefa426e03737639cd8b61f78d59f70f70f5503
Ra Lin <rasheedlindsey@live.com>
2023-11-29 18:56:59 -0500

    Reviews seeders donzo

---
+++ backend/db/seeders/20231129235329-demo-review.js
@@ -0,0 +1,41 @@
+'use strict';
+const { Booking } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await Spot.bulkCreate([
+      {
+        spotId: 1,
+        userId: 1,
+        review: "This fake spot deserves a fake review!",
+        stars: 3
+      },
+      {
+        spotId: 2,
+        userId: 2,
+        review: "I can't review this spot, it's still unknown.",
+        stars: 1
+      },
+      {
+        spotId: 3,
+        userId: 3,
+        review: "This spot is hot, Hot, HOTTT!!!",
+        stars: 5
+      },
+    ], { validate: true})
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Reviews';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [1, 2, 3] }
+    }, {});
+  }
+};
