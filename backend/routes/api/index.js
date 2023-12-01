21d4605694d063e74794078bc625464e4dc08288
Ra Lin <rasheedlindsey@live.com>
2023-11-30 23:46:37 -0500

    Can get all spots, just need to add previewImage and avgRating

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
+const spotsRouter = require('./spots.js')
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 
@@ -36,6 +37,8 @@ router.use(restoreUser);
 
   router.use('/users', usersRouter);
 
+  router.use('/spots', spotsRouter)
+  
   router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
   });
---
+++ backend/routes/api/spots.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { Spot, Review, Image } = require("../../db/models");
+const { Op } = require("sequelize");
+const router = express.Router();
+const { setTokenCookie, restoreUser } = require("../../utils/auth");
+
+router.get("/", async (req, res, next) => {
+  let allSpots = [];
+
+  allSpots = await Spot.findAll();
+  res.status(200);
+  res.json(allSpots);
+});
+
+module.exports = router;
