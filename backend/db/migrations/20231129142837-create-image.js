7f6a7aa9335d07c2c7039910d93b7dfbad74d241
Ra Lin <rasheedlindsey@live.com>
2023-12-01 22:26:10 -0500

    More database updates

--- backend/db/migrations/20231129142837-create-image.js
+++ backend/db/migrations/20231129142837-create-image.js
@@ -24,6 +24,9 @@ module.exports = {
         imagePreview: {
           type: Sequelize.BOOLEAN,
         },
+        url: {
+          type: Sequelize.STRING,
+        },
         createdAt: {
           allowNull: false,
           type: Sequelize.DATE,
