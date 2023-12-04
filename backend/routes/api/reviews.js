644c8316caed7496320d45e01c72c32ede812285
Ra Lin <rasheedlindsey@live.com>
2023-12-04 01:30:37 -0500

    Delete a spot complete

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -13,38 +13,58 @@ const validateReview = [
     .withMessage("Review text is required"),
   check("stars")
     .exists({ checkFalsy: true })
-    .isFloat({ min: 1, max: 5})
+    .isFloat({ min: 1, max: 5 })
     .withMessage("Stars must be an integer from 1 to 5"),
   handleValidationErrors,
 ];
 
 // Edit user's reviews
 router.put("/:reviewId", requireAuth, validateReview, async (req, res) => {
-  try {
-    const { review, stars } = req.body;
-    const reviewId = req.params.reviewId;
-    const updateReview = await Review.findOne({
-      where: {
-        id: reviewId,
-      },
-    });
-    if (req.user) {
-      if (updateReview && updateReview.userId === req.user.id) {
-        updateReview.set({
-          review: review,
-          stars: stars,
-        });
-        await updateReview.save();
-        res.json(updateReview);
-      } else {
-        res.status(404);
-        res.json({
-          message: "Review couldn't be found",
-        });
-      }
+  const { review, stars } = req.body;
+  const reviewId = req.params.reviewId;
+  const updateReview = await Review.findOne({
+    where: {
+      id: reviewId,
+    },
+  });
+  if (req.user) {
+    if (updateReview && updateReview.userId === req.user.id) {
+      updateReview.set({
+        review: review,
+        stars: stars,
+      });
+      await updateReview.save();
+      res.json(updateReview);
+    } else {
+      res.status(404);
+      res.json({
+        message: "Review couldn't be found",
+      });
     }
-  } catch (error) {
-    console.log(error);
+  }
+});
+
+// Delete a user's review
+router.delete("/:reviewId", async (req, res) => {
+  const reviewId = req.params.reviewId;
+  const deleteReview = await Review.findOne({
+    where: {
+      id: reviewId,
+    },
+  });
+  if (req.user) {
+    if (deleteReview && deleteReview.userId === req.user.id) {
+      deleteReview.destroy();
+
+      res.json({
+        message: "Successfully deleted",
+      });
+    } else {
+      res.status(404);
+      res.json({
+        message: "Review couldn't be found",
+      });
+    }
   }
 });
 
