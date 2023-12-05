ebd855215c3f6325d6c338fb4d1ebe5c375aa1f3
Ra Lin <rasheedlindsey@live.com>
2023-12-04 20:32:37 -0500

    Working on error handling in adding review images

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { Review } = require("../../db/models");
+const { Review, Image } = require("../../db/models");
 const { requireAuth } = require("../../utils/auth");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
@@ -17,6 +17,36 @@ const validateReview = [
   handleValidationErrors,
 ];
 
+// Adds an image to a review
+router.post("/:reviewId/images", requireAuth, async (req, res) => {
+  const reviewId = req.params.reviewId;
+  const { url } = req.body;
+  const findReview = await Review.findOne({
+    where: { id: reviewId, userId: req.user.id },
+  });
+  const reviewImageList = []
+  reviewImageList.push(findReview.toJSON())
+
+  if (!findReview) {
+    res.status(404);
+    res.json({
+      message: "Review couldn't be found",
+    });
+  } else if (reviewImageList.length === 10) {
+    res.status(403);
+    res.json({
+      message: "Maximum number of images for this resource was reached",
+    });
+  } else if (findReview.userId === req.user.id) {
+    const newReviewImage = Image.build({
+      url: url,
+    });
+    await newReviewImage.save();
+    res.status(201);
+    res.json(newReviewImage);
+  }
+});
+
 // Edit user's reviews
 router.put("/:reviewId", requireAuth, validateReview, async (req, res) => {
   const { review, stars } = req.body;
@@ -27,20 +57,19 @@ router.put("/:reviewId", requireAuth, validateReview, async (req, res) => {
     },
   });
 
-    if (updateReview && updateReview.userId === req.user.id) {
-      updateReview.set({
-        review: review,
-        stars: stars,
-      });
-      await updateReview.save();
-      res.json(updateReview);
-    } else {
-      res.status(404);
-      res.json({
-        message: "Review couldn't be found",
-      });
-    }
-  
+  if (updateReview && updateReview.userId === req.user.id) {
+    updateReview.set({
+      review: review,
+      stars: stars,
+    });
+    await updateReview.save();
+    res.json(updateReview);
+  } else {
+    res.status(404);
+    res.json({
+      message: "Review couldn't be found",
+    });
+  }
 });
 
 // Delete a user's review
