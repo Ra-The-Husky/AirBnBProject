37126e05f0a49a803fd2aa732b84db587616568a
Ra Lin <rasheedlindsey@live.com>
2024-01-15 13:44:12 -0500

    Login actions completed, tested, working

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,8 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import configureStore from "./store/store";
-import { restoreCSRF, csrfFetch } from './store/csrf';
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
@@ -12,11 +13,12 @@ const store = configureStore();
 //   window.store = store;
 // }
 
-if (import.meta.env.MODE !== 'production') {
+if (import.meta.env.MODE !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
